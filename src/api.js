2a555a6e1d0f156b0cbc573bd98a6b7196ed5f12
Michael Fox <mikefox17@gmail.com>
2021-02-11 10:39:27 -0500

    Initial setup and redux store created

---
+++ src/api.js
@@ -0,0 +1,37 @@
+//base url
+
+const base_url = 'https://api.rawg.io/api/';
+
+//get date
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`;
+    } else {
+        return month;
+    }
+};
+getCurrentMonth();
+
+const getCurrentDay = () => {
+    const day = new Date().getDate() + 1;
+    if (day < 10) {
+        return `0${day}`;
+    } else {
+        return day;
+    }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesURL());
